1b5556a6b2184e7c2d26d85e2acf8c1a076ca589
Dominik Wagner <wagnerdo@fim.uni-passau.de>
2014-12-20 18:54:29 +0100

    add exception throw for negative

--- Assignment5/src/com/acertainbank/client/AccountManagerProxy.java
+++ Assignment5/src/com/acertainbank/client/AccountManagerProxy.java
@@ -1,5 +1,63 @@
 package com.acertainbank.client;
 
-public class AccountManagerProxy {
+import com.acertainbank.*;
+
+public class AccountManagerProxy implements AccountManager {
+
+	@Override
+	public void credit(int branchId, int accountId, double amount)
+			throws InexistentBranchException, InexistentAccountException,
+			NegativeAmountException {
+		if (amount < 0) {
+			throw new NegativeAmountException(amount);
+		} else if (branchId < 0 || ) {
+			throw new InexistentBranchException(branchId);
+		} else if (accountId < 0 || ) {
+			throw new InexistentAccountException(accountId);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void debit(int branchId, int accountId, double amount)
+			throws InexistentBranchException, InexistentAccountException,
+			NegativeAmountException {
+		if (amount < 0) {
+			throw new NegativeAmountException(amount);
+		} else if (branchId < 0 || ) {
+			throw new InexistentBranchException(branchId);
+		} else if (accountId < 0 || ) {
+			throw new InexistentAccountException(accountId);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void transfer(int branchId, int accountIdOrig, int accountIdDest,
+			double amount) throws InexistentBranchException,
+			InexistentAccountException, NegativeAmountException {
+		if (amount < 0) {
+			throw new NegativeAmountException(amount);
+		} else if (branchId < 0 || ) {
+			throw new InexistentBranchException(branchId);
+		} else if (accountId < 0 || ) {
+			throw new InexistentAccountException(accountId);
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public double calculateExposure(int branchId)
+			throws InexistentBranchException {
+		if (branchId < 0 || ){
+			throw new InexistentBranchException(branchId);
+
+		}
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
